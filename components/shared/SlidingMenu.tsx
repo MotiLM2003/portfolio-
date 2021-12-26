faea9a65409e550cd4229758617ccfd9042074cd
Moti Elmakies <motiphone2003@gmail.com>
2021-12-26 21:49:07 +0200

    :bulb: sliding menu: added helper function and some css

--- components/shared/SlidingMenu.tsx
+++ components/shared/SlidingMenu.tsx
@@ -4,22 +4,45 @@ interface Props {
   children?: JSX.Element | JSX.Element[];
 }
 
+const loopOverItems = (menuItems: any, action: Function) => {
+  let index = 0;
+  const items = menuItems?.children;
+  for (let item of items as HTMLCollection) {
+    if (item.classList.contains('sm-item')) {
+      const li = items[index];
+      action(li);
+    }
+    index++;
+  }
+};
+
 export const SlidingMenu: React.FC<Props> = ({ children }) => {
+  const [selectedMenu, setSelectedMenu] = useState(null);
+
   useEffect(() => {
     const menuItems = containerRef.current?.children[0];
-    // console.log(menuItems?.children.length);
-
     if (menuItems) {
-      for (let item of menuItems?.children as any) {
-        // console.log(item);
-        console.log(item.classList.contains('sm-item'));
-      }
+      loopOverItems(menuItems, (li: any) => {
+        li.addEventListener('click', (e: Event) => {
+          console.log(li);
+        });
+      });
     }
-  }, [children]);
+
+    return () => {
+      loopOverItems(menuItems, (li: HTMLElement) => {
+        li.removeEventListener('click', (e: Event) => {
+          console.log(e);
+        });
+      });
+    };
+  }, []);
   const containerRef = useRef<HTMLDivElement | null>(null);
   return (
     <div className='sliding-menu' ref={containerRef}>
       {children && children}
+      <div className='selected-item'>test</div>
+      <div></div>
     </div>
   );
 };
